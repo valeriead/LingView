b9a76f76f2bc6ebe663f1f52cbe7ee8ff87c8c76
valeriead <63419974+valeriead@users.noreply.github.com>
2020-06-28 11:16:57 -0400

    Update edit.js

--- preprocessing/edit.js
+++ preprocessing/edit.js
@@ -1,3 +1,229 @@
+const fs = require("fs")
+const inquirer = require('inquirer')
+let obj = JSON.parse(fs.readFileSync("data/index.json", "utf8"));
+
+let data = obj["1e3bff88-29b9-4b3f-be34-bffc38624bac"]
+
+function main() {
+  inquirer.prompt([
+    {
+      "type": "list",
+      "name": "valueToEdit",
+      "message": "What do you want to edit?",
+      "choices": [
+        "audio",
+        "video",
+        "title",
+        "description",
+        "genre",
+        "author",
+        "glosser",
+        "date recorded",
+        "source"
+      ]
+    },
+    // mp3
+    {
+      "type": "input",
+			"name": "audio",
+			"message": "Name of mp3 file:",
+			"default": data["media"]["audio"],
+			"when":
+				function(answers) {
+          const condition = true && answers.valueToEdit === 'audio'
+          // const condition = hasTimestamps(filename) && answers.valueToEdit === "audio"
+					return condition;
+				},
+			// "validate":	
+			// 	function(response) {
+			// 		const media_files = fs.readdirSync("data/media_files");
+			// 		if (media_files.indexOf(response) >= 0 || response === "") {
+			// 			return true;
+			// 		} else if (response === "blank") { // TODO: replace "blank" with "" in then()
+			// 			return true;
+			// 		} else {
+			// 			return "That file doesn't exist in your media_files directory! Please be aware that filenames are case-sensitive and require an extension. Type 'blank' to leave the file blank.";
+			// 		}
+			// 	}
+    },
+    // mp4
+    {
+      "type": "input",
+			"name": "video",
+			"message": "Name of mp4 file:",
+			"default": data["media"]["video"],
+			"when":
+        function(answers) {
+          const condition = true && answers.valueToEdit === 'video'
+          // const condition = hasTimestamps(filename) && answers.valueToEdit === "audio"
+          return condition;
+        },
+			// "validate":	
+			// 	function(response) {
+			// 		const media_files = fs.readdirSync("data/media_files");
+			// 		if (media_files.indexOf(response) >= 0 || response === "") {
+			// 			return true;
+			// 		} else if (response === "blank") {
+			// 			return true;
+			// 		} else {
+			// 			return "That file doesn't exist in your media_files directory! Please be aware that filenames are case-sensitive and require an extension. Type 'blank' to leave the file blank.";
+			// 		}
+			// 	}
+    },
+    // edit title?
+    {
+			"type": "input", 
+			"name": "title",
+      "message": "Title:",
+      "default": data["title"]["_default"],
+      "when":
+        function(answers) {
+          return answers.valueToEdit === "title"
+        }
+    },
+    // edit description?
+    {
+			"type": "confirm", 
+			"name": "desc_edit",
+			"message": "Edit description?",
+			"default": false,
+			"when": 
+				function(answers) {
+          if (data["description"] && answers.valueToEdit === "description") {
+						console.log("You've already entered a description: " + '"' + data["description"] + '"');
+						return true;
+					} else {
+						return false;
+					}
+				}
+    },
+    // description editor (probably using Vim)
+		{
+			"type": "editor", 
+			"name": "description",
+			"message": " ", // cannot be empty :(
+			"default": data["description"],
+			"when": 
+				function(answers) {
+					return (answers["desc_edit"]);
+				}
+		},
+		// description creator
+		{
+			"type": "input", 
+			"name": "description",
+			"message": "Enter a description:",
+			"when": 
+				function(answers) {
+					return (data["description"] === "" && answers.valueToEdit === "description");
+				}
+		},
+		// genre
+		{
+			"type": "list", 
+			"name": "genre",
+			"message": "Select a genre:",
+			"choices": ["Nonfiction", "Fiction", ""],
+      "default": data["genre"],
+      "when":
+        function(answers) {
+          return answers.valueToEdit === "genre"
+        }
+		},
+		// author
+		{
+			"type": "input", 
+			"name": "author",
+			"message": "Author:",
+      "default": data["author"],
+      "when":
+        function(answers) {
+          return answers.valueToEdit === "author"
+        }
+		},
+		// glosser
+		{
+			"type": "input", 
+			"name": "glosser",
+			"message": "Who glossed it:",
+      "default": data["glosser"],
+      "when":
+        function(answers) {
+          return answers.valueToEdit === "glosser"
+        }
+		},
+		// date recorded
+		{
+			"type": "input", 
+			"name": "date_created",
+			"message": "Date of creation (mm/dd/yyyy):",
+      "default": data["date_created"],
+      "when":
+        function(answers) {
+          return answers.valueToEdit === "date recorded"
+        }
+		},
+		// source
+		{
+			"type": "input", 
+			"name": "source",
+			"message": "Source:",
+      "default": data["source"]["_default"],
+      "when":
+        function(answers) {
+          return answers.valueToEdit === "source"
+        }
+		}
+  ]).then(function (answers) {
+    if (answers["audio"] && answers["audio"] == "blank") {
+			data["media"]["audio"] == "";
+		} else if (answers["audio"]) {
+			data["media"]["audio"] = answers["audio"];
+		}
+		if (answers["video"] && answers["video"] == "blank") {
+			data["media"]["video"] == "";
+		} else if (answers["video"]) {
+			data["media"]["video"] = answers["video"];
+		}
+		data["timed"] = (data["media"]["audio"] != "") || (data["media"]["video"] != "");
+		
+		if (answers["description"]) {
+			data["description"] = answers["description"];
+    }
+    
+    if (answers["title"]) {
+      data["title"]["_default"] = answers["title"]
+    }
+    
+    if (answers["genre"]) {
+      data["genre"] = answers["genre"]
+    }
+
+    if (answers["author"]) {
+      data["author"] = answers["author"]
+    }
+
+    if (answers["glosser"]) {
+      data["glosser"] = answers["glosser"]
+    }
+    
+    if (answers["date_created"]) {
+      data["date_created"] = answers["date_created"]
+    }
+    
+    if (answers["source"]) {
+      data["source"]["_default"] = answers["source"]
+    }
+
+    fs.writeFileSync("data/index.json", JSON.stringify(obj, null, 2));
+  })
+}
+
+main()
+
+
+// EDIT.JS !!!!
+
 const fs = require("fs");
 const prompt = require("prompt");
 const inquirer = require("inquirer"); // edited the node module for this
@@ -70,7 +296,7 @@ function main(callback) {
           const condition = hasTimestamps(filename) && answers.valueToEdit === "audio"
 					return condition;
 				},
-			"validate":
+			"validate":	
 				function(response) {
 					const media_files = fs.readdirSync("data/media_files");
 					if (media_files.indexOf(response) >= 0 || response === "") {
@@ -93,7 +319,7 @@ function main(callback) {
           const condition = hasTimestamps(filename) && answers.valueToEdit === "audio"
           return condition;
         },
-			"validate":
+			"validate":	
 				function(response) {
 					const media_files = fs.readdirSync("data/media_files");
 					if (media_files.indexOf(response) >= 0 || response === "") {
@@ -107,7 +333,7 @@ function main(callback) {
     },
     // edit title?
     {
-			"type": "input",
+			"type": "input", 
 			"name": "title",
       "message": "Title:",
       "default": data["title"]["_default"],
@@ -118,11 +344,11 @@ function main(callback) {
     },
     // edit description?
     {
-			"type": "confirm",
+			"type": "confirm", 
 			"name": "desc_edit",
 			"message": "Edit description?",
 			"default": false,
-			"when":
+			"when": 
 				function(answers) {
           if (data["description"] && answers.valueToEdit === "description") {
 						console.log("You've already entered a description: " + '"' + data["description"] + '"');
@@ -134,28 +360,28 @@ function main(callback) {
     },
     // description editor (probably using Vim)
 		{
-			"type": "editor",
+			"type": "editor", 
 			"name": "description",
 			"message": " ", // cannot be empty :(
 			"default": data["description"],
-			"when":
+			"when": 
 				function(answers) {
 					return (answers["desc_edit"]);
 				}
 		},
 		// description creator
 		{
-			"type": "input",
+			"type": "input", 
 			"name": "description",
 			"message": "Enter a description:",
-			"when":
+			"when": 
 				function(answers) {
 					return (data["description"] === "" && answers.valueToEdit === "description");
 				}
 		},
 		// genre
 		{
-			"type": "list",
+			"type": "list", 
 			"name": "genre",
 			"message": "Select a genre:",
 			"choices": ["Nonfiction", "Fiction", ""],
@@ -167,7 +393,7 @@ function main(callback) {
 		},
 		// author
 		{
-			"type": "input",
+			"type": "input", 
 			"name": "author",
 			"message": "Author:",
       "default": data["author"],
@@ -178,7 +404,7 @@ function main(callback) {
 		},
 		// glosser
 		{
-			"type": "input",
+			"type": "input", 
 			"name": "glosser",
 			"message": "Who glossed it:",
       "default": data["glosser"],
@@ -189,7 +415,7 @@ function main(callback) {
 		},
 		// date recorded
 		{
-			"type": "input",
+			"type": "input", 
 			"name": "date_created",
 			"message": "Date of creation (mm/dd/yyyy):",
       "default": data["date_created"],
@@ -200,7 +426,7 @@ function main(callback) {
 		},
 		// source
 		{
-			"type": "input",
+			"type": "input", 
 			"name": "source",
 			"message": "Source:",
       "default": data["source"]["_default"],
@@ -221,15 +447,15 @@ function main(callback) {
 			data["media"]["video"] = answers["video"];
 		}
 		data["timed"] = (data["media"]["audio"] != "") || (data["media"]["video"] != "");
-
+		
 		if (answers["description"]) {
 			data["description"] = answers["description"];
     }
-
+    
     if (answers["title"]) {
       data["title"]["_default"] = answers["title"]
     }
-
+    
     if (answers["genre"]) {
       data["genre"] = answers["genre"]
     }
@@ -241,11 +467,11 @@ function main(callback) {
     if (answers["glosser"]) {
       data["glosser"] = answers["glosser"]
     }
-
+    
     if (answers["date_created"]) {
       data["date_created"] = answers["date_created"]
     }
-
+    
     if (answers["source"]) {
       data["source"]["_default"] = answers["source"]
     }
@@ -259,5 +485,5 @@ function update() {
 	DB["index"] = obj;
 	fs.writeFileSync("data/database.json", JSON.stringify(DB, null, 2));
 	console.log("📤" + "  " + "Metadata edit complete.");
-	console.log("\nYou've successfully edited the metadata. However, this will not be displayed on the site until you rebuild the databases and site. (You can do both using the \"quick-build-online\" or \"quick-build-offline\" npm script; for more info: https://github.com/BrownCLPS/LingView/wiki)");
+	console.log("\nYou've successfully edited the metadata. However, this will not be displayed on the site until you run the rebuild.js script. You can run this script from the root directory with the command 'node preprocessing/rebuild.js'. We recommend doing this immediately.");
 }
